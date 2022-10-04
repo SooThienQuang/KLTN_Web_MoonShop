cea00f4e7b13ea22e1d63689f6cd74bec3f6cdf9
soothienquang <thienquangpro1221@gmail.com>
2022-10-04 20:47:52 +0700

    [Quang] Commiit lần 1

---
+++ SourceCode/MoonShop/MoonShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MoonShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoonShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DBCosmeticEntities db = new DBCosmeticEntities();
+        public ActionResult Index()
+        {
+            return View(db.Tbl_Product.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Tbl_Product b)
+        {
+            db.Tbl_Product.Add(b);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
