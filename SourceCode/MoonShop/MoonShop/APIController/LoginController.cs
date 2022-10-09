37e8676670dd89a9d0655726063587689fdc1a41
soothienquang <thienquangpro1221@gmail.com>
2022-10-09 15:08:10 +0700

    [Quang] Commit codel lan 02

---
+++ SourceCode/MoonShop/MoonShop/APIController/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MoonShop.Models;
+namespace MoonShop.Controllers.APIController
+{
+    public class LoginController : ApiController
+    {
+        // GET: api/Login
+        DBCosmeticEntities db = new DBCosmeticEntities();
+        public IEnumerable<Tbl_Product> Get()
+        {
+            return db.Tbl_Product.ToList();
+        }
+
+        // GET: api/Login/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Login
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Login/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Login/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SourceCode/MoonShop/MoonShop/Controllers/CaroselController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoonShop.Controllers
+{
+    public class CaroselController : Controller
+    {
+        // GET: Carosel
+        public ActionResult CaroselBaner()
+        {
+            return PartialView();
+        }
+        public ActionResult CaroselBanner()
+        {
+            return PartialView();
+        }
+    }
+}
