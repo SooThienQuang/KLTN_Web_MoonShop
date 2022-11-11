1c07913ec84b5b9bc7ce7440b6c021235d72ae81
soothienquang <thienquangpro1221@gmail.com>
2022-11-11 09:09:34 +0700

    2022-11-11

--- SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Controllers/ShoppingCartController.cs
+++ SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KLTN_Web_MoonShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace KLTN_Web_MoonShop.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        DBCosmeticEntities db = new DBCosmeticEntities();
         // GET: ShoppingCart
         public ActionResult AddOrUpdate()
         {
@@ -16,7 +18,15 @@ namespace KLTN_Web_MoonShop.Controllers
         }
         public ActionResult AllCart()
         {
-            return View();
+            Customer user = Session["user"] as Customer;
+            List<CartDetail> lst = new List<CartDetail>();
+            if (user!=null)
+            {
+              Cart cart=  db.Carts.FirstOrDefault(n => n.customerID == user.customerID);
+               lst = db.CartDetails.Where(n => n.cartID == cart.cartID).ToList();
+               return View(lst);
+            }    
+            return View(lst);
         }
         public ActionResult EmryCart()
         {
---
+++ SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Models/CartClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KLTN_Web_MoonShop.Models
+{
+    public class CartClass
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int soluong { get; set; }
+        public float gia { get; set; }
+        public float thanhtien { get; set; }
+    }
+}
