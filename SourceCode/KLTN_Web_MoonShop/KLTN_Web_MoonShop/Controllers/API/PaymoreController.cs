791a2ace630d3560a53f3ca00e00c4d33adcf4ae
soothienquang <thienquangpro1221@gmail.com>
2022-11-14 21:15:46 +0700

    2022-11-14

---
+++ SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Controllers/API/PaymoreController.cs
@@ -0,0 +1,27 @@
+﻿using KLTN_Web_MoonShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KLTN_Web_MoonShop.Controllers.API
+{
+    public class request
+    {
+        public string chuoi { get; set; }
+    }
+    public class PaymoreController : ApiController
+    {
+        DBCosmeticEntities db = new DBCosmeticEntities();
+        [HttpPost]
+        public List<Product> paymore(request data)
+        {
+            List<Product> lst = new List<Product>();
+            //Product result = db.Products.FirstOrDefault();
+            //lst.Add(result);
+            return db.Products.ToList(); 
+        }
+    }
+}
--- SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Controllers/PaymentController.cs
+++ SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Controllers/PaymentController.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using KLTN_Web_MoonShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 
 namespace KLTN_Web_MoonShop.Controllers
 {
+    public class request
+    {
+        public string chuoi { get; set; }
+    }
+
     public class PaymentController : Controller
     {
+        DBCosmeticEntities db = new DBCosmeticEntities();
         // GET: Payment
         public ActionResult Checkout()
         {
@@ -17,5 +25,62 @@ namespace KLTN_Web_MoonShop.Controllers
         {
             return View();
         }
+        public ActionResult Paymore(string id)
+        {
+            Customer user = Session["user"]as Customer;
+            //gio hang
+            Cart cart = db.Carts.FirstOrDefault(n => n.customerID == user.customerID);
+            //danh sach tat ca chi tiet gio hang
+            List<CartDetail> lstcartdeall = db.CartDetails.Where(n => n.cartID == cart.cartID).ToList();
+            //chi tiet san pham da chon tu gio hang
+            List<CartDetail> lstd = new List<CartDetail>();
+            List<Product> lst = new List<Product>();
+            List<long> proid = new List<long>();
+            if(id.Equals("getallcartqnt"))
+            {
+                if(user!=null)
+                {
+                    foreach(CartDetail detail in lstcartdeall)
+                    {
+                        Product d = db.Products.FirstOrDefault(n => n.productID == detail.productID);
+                        lst.Add(d);
+                    }    
+                }
+                lstd = lstcartdeall;
+                ViewBag.CartDetail = lstd;
+                ViewBag.allMoney = lstd.Sum(n => n.cartMoney);
+            }
+            else
+            {
+                string[] a = id.Split(',');
+                var unique_items = new HashSet<string>(a);
+                foreach (string s in unique_items)
+                {
+                    if (s != null && s.Equals("") == false)
+                    {
+                        proid.Add(long.Parse(s));
+                    }
+                }
+
+                foreach (long pro in proid)
+                {
+                    Product pro1 = db.Products.FirstOrDefault(n => n.productID == pro);
+                    lst.Add(pro1);
+                   
+                    foreach(var item in lstcartdeall)
+                    {
+                        if(item.productID==pro)
+                        {
+                            lstd.Add(item);
+                        }    
+                    }    
+                }
+                ViewBag.CartDetail = lstd;
+                ViewBag.allMoney = lstd.Sum(n => n.cartMoney);
+            }    
+            
+            return PartialView(lst);
+        }
+
     }
 }
