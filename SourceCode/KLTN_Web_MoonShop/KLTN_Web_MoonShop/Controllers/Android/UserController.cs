73d555327d4ea45a68bb2b338a2d05224ac7dac2
soothienquang <thienquangpro1221@gmail.com>
2022-11-28 08:33:25 +0700

    no message

--- SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Controllers/Android/UserController.cs
+++ SourceCode/KLTN_Web_MoonShop/KLTN_Web_MoonShop/Controllers/Android/UserController.cs
@@ -68,5 +68,86 @@ namespace KLTN_Web_MoonShop.Controllers.Android
                 return null;
             }
         }
+        [Route("postuser")]
+        [HttpGet]
+        public string signup(string fullname,string phone, string password)
+        { 
+                string pass = md5.CreateMD5(password);
+                Customer customer = db.Customers.FirstOrDefault(n => n.customerUserName == phone && n.isActive == 1);
+                if (customer != null)
+                {
+                    return "Tài khoản đã tồn tại";
+                }
+                else
+                {
+                    Customer cs = new Customer();
+                    long id = long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss"));
+                    cs.customerID = id;
+                    cs.customerUserName = (phone).Trim();
+                    cs.customerPassword = md5.CreateMD5(password);
+                    cs.customerPhoto = "Sample_User_Icon.png".Trim();
+                    cs.isActive = 1;
+                    cs.dateCreate = DateTime.Now;
+                    db.Customers.Add(cs);
+                    db.SaveChanges();
+                    //thông báo đăng kí thành công
+                    Notification noti = new Notification();
+                    long idnoti = long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss"));
+                    noti.notiID = idnoti;
+                    noti.receiveUserID = id;
+                    noti.receiveUserFullName = fullname;
+                    noti.title = "Chúc mừng bạn đã đăng kí tài khoản thành công";
+                    noti.message = "Cám ơn bạn đã tin tưởng Shop , mong bạn có một trải nghiệm tốt bằng các dịch vụ của chúng mình !";
+                    noti.image = "check.jpg";
+                    noti.menutype = 1;
+                    noti.isRead = 0;
+                    db.Notifications.Add(noti);
+                    db.SaveChanges();
+                    //thông báo giảm giá khách hàng mới
+                    Notification noti2 = new Notification();
+                    long idnoti2 = long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss"));
+                    noti2.notiID = idnoti2;
+                    noti2.receiveUserID = id;
+                    noti2.receiveUserFullName = fullname;
+                    noti2.title = "Giảm giá cho khách hàng mới đến";
+                    noti2.message = "Tặng bạn voucher giảm giá 20% ! Click để xem chi tiết";
+                    noti2.image = "discount20.jpg";
+                    noti2.menutype = 1;
+                    noti2.isRead = 0;
+                    db.Notifications.Add(noti2);
+                    db.SaveChanges();
+                    return "Đăng kí thành công";
+                }
+            return " Đăng kí thất bại";
+            
+        }
+        [Route("getdetailpro")]
+        [HttpGet]
+        public IEnumerable<ProductData> getdetailpro(string id)
+        {
+            long proid = long.Parse(id);
+            List<ProductData> lst = new List<ProductData>();
+            Product product = db.Products.FirstOrDefault(n => n.isActive == 1 && n.productID == proid);
+                ProductDetail pd = db.ProductDetails.FirstOrDefault(n => n.ProductID == product.productID);
+                ProductData pdd = new ProductData();
+                pdd.productID = product.productID;
+                pdd.productName = product.productName;
+                pdd.productQuantity = product.productQuantity;
+                pdd.productPrice = product.productPrice;
+                pdd.productDescribe = product.productDescribe;
+                pdd.productImage = product.productImage;
+                pdd.productTypeID = product.productTypeID;
+                pdd.isActive = product.isActive;
+                pdd.Size = pd.Size;
+                pdd.Color = pd.Color;
+                pdd.img1 = pd.img1;
+                pdd.img2 = pd.img2;
+                pdd.img3 = pd.img3;
+                pdd.origin = pd.origin;
+                pdd.brand = pd.brand;
+                pdd.desciption = pd.desciption;
+                lst.Add(pdd);
+            return lst;
+        }
     }
 }
